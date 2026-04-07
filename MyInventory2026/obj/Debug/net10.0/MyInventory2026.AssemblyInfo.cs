bd406d2a3e78e7b749911267ebdae4d22dc16edd
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-04-07 17:23:39 -0500

    feat: db connection

--- MyInventory2026/obj/Debug/net10.0/MyInventory2026.AssemblyInfo.cs
+++ MyInventory2026/obj/Debug/net10.0/MyInventory2026.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyInventory2026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6566f107e53ce66c742990eb35740bacbeeea9b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyInventory2026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyInventory2026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ MyInventory2026/src/modules/provider/domain/valueObject/ProviderName.cs
@@ -0,0 +1,6 @@
+namespace MyInventory2026.src.modules.provider.domain.valueObject;
+
+public record class ProviderName
+{
+
+}
